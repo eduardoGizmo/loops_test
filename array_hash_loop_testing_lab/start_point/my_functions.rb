7defff32eaadefed6f8a3f7fc54ba399416003a4
Eduardo Gizmo <eduardogimenezmoreno@gmail.com>
2019-10-16 15:33:44 +0100

    Initial commit - first test success

---
+++ array_hash_loop_testing_lab/start_point/my_functions.rb
@@ -0,0 +1,8 @@
+citrus_fruits = [ "lemon", "orange" ]
+berries = [ "strawberry", "blackberry", "raspberry" ]
+
+def add_array_lengths(array_1, array_2)
+  return array_1.length + array_2.length
+end
+
+p add_array_lengths(citrus_fruits, berries)
